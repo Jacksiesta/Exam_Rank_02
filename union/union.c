95d9dd7dfed6673fba5b19f80d9bd3f6eb44c619
jacksiesta <jacqueline.herrald@gmail.com>
2020-03-07 22:24:39 +0100

    ca a l'air de marcher

--- union/union.c
+++ union/union.c
@@ -6,71 +6,44 @@
 /*   By: jherrald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/07 20:58:02 by jherrald          #+#    #+#             */
-/*   Updated: 2020/03/07 22:11:51 by jherrald         ###   ########.fr       */
+/*   Updated: 2020/03/07 22:24:25 by jherrald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-// ret 1 -> char exists || ret 0 -> char doesn't exist
-
-int		search_str(char c, char *str)
-{
-	int x;
-
-	x = 0;
-	while (str[x])
-	{
-		if (str[x] == c)
-			return (1);
-		x++;
-	}
-	return (0);
-}
 
 int 	main(int ac, char **av)
 {
-	int 	x;
-	int		y;
-	char	tab[36];
+	int		x;
+	int 	y;
+	char	tab[266];
 
-	x = 0; // char numb x
-	y = 0; // argm numb y
-	while (x < 36)
+	if (ac != 3)
+	{
+		write(1, "\n", 1);
+		return (1);
+	}
+	x = 0;
+	while (x < 266)
 	{
 		tab[x] = 0;
 		x++;
 	}
 	x = 1;
-	if (ac != 2)
+	while (x <= 2)
 	{
-		while (y < 2)
+		y = 0;
+		while (av[x][y])
 		{
-			y = 0;
-			while (av[x][y])
+			if (tab[(unsigned char)av[x][y]] == 0)
 			{
-				if (tab[(unsigned char)av[x][y]] == 1)
-				{
-					y++;
-				}
-				else
-				{
-					write(1, &av[x][y], 1);
-					//ft_putchar(av[x][y]);
-					tab[(unsigned char)av[x][y]] = 1;
-					y++;
-				}
+				write(1, &av[x][y], 1);
+				tab[(unsigned char)av[x][y]] = 1;
 			}
-			x++;
+			y++;
 		}
+		x++;
 	}
-	ft_putchar('\n');	
+	write(1, "\n", 1);
 	return (0);
 }
